2be037b2c8774fc5a1ff14d5ede3edd91a35df4d
Derzam <derlymoreira192@gmail.com>
2021-09-24 19:48:44 -0500

    PRUEBA

---
+++ prueba.final/prueba/final2/Main.java
@@ -0,0 +1,26 @@
+package prueba.final2;
+
+import java.util.Scanner;
+
+public class Main {
+//
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int opcion = 0;
+		Scanner teclado = new Scanner(System.in);
+		
+		do {
+		
+		System.out.println("############################");
+		System.out.println("Aseguradora del Sur, elija una opcion");
+		System.out.println("1-Ingresar Poliza");
+		System.out.println("2-Actualizar valor ");
+		System.out.println("3-Consultar Poliza");
+		System.out.println("Imprimir Reporte");
+		System.out.println("5-Salir");
+		System.out.println("#############################");
+		
+		}while(opcion!=5);
+	}
+
+}
